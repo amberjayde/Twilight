363059ea69aa748f0a7971d6c69891ec1fe8e3c6
AmberCeleste <Jayde@10.0.0.89>
2017-07-05 22:41:47 -0700

    MVC Implemented

--- src/twilight/Controller.java
+++ src/twilight/Controller.java
@@ -5,17 +5,39 @@
  */
 package twilight;
 
+import java.awt.event.ActionEvent;
+
 /**
  *
  * @author Jayde
  */
-public class Controller {
+public class Controller implements java.awt.event.ActionListener {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    Model model;
+    View  view;
+    
+    Controller() {System.out.println ("Controller()");} //println for comfirmation
+    Controller(Model m, View v)
+    {
+        this.model = m; this.view = v;
+        this.view.addListener(this); //passes controller to the view
+        System.out.println("Controller running with new model and new view");
+    }
+    
+    public void addModel(Model m)
+    {
+            System.out.println("Controller: adding model");
+		this.model = m;
+    }
+    public void addView(View v)
+    {
+		System.out.println("Controller: adding view");
+		this.view = v;
+    } //addView()
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
---
+++ src/twilight/RunGame.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twilight;
+
+import com.sun.glass.ui.Application;
+
+/**
+ *
+ * @author Jayde
+ */
+class RunGame {
+    
+    public RunGame()
+    {
+        //create Model and View2
+        Model myModel = new Model();
+        View myView = new View();
+        
+        //create Contoller. Tell it about Model and View2.
+        Controller myController = new Controller(myModel,myView);
+        myView.setVisible(true);
+        
+        /* Controller myController = new Controller(); //empty constructor
+        myController.addModel(myModel);
+        myController.addView(myView);
+        */
+        
+        //tell View2 about Controller
+       // myView.addController(myController);
+    }
+    
+}
